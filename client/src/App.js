96d74aa5c49b7b49cb21873352c06730a0faeb48
fbzr <fabricio.spb@gmail.com>
2020-03-13 10:33:32 -0700

    create Chart with players with more than 10 searches

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import SearchesList from './components/SearchesList';
 import { Container } from '@material-ui/core';
 import DarkModeToggle from './components/DarkModeToggle';
 import './App.scss';
+import Chart from './components/Chart';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1>App</h1>
       <DarkModeToggle />
       <SearchesList />
+      <Chart />
     </Container>
   );
 }
--- client/src/fetchdata/fetchData.js
+++ client/src/fetchdata/fetchData.js
@@ -10,3 +10,14 @@ export default async () => {
         }
     }
 }
+
+export const getMoreThanTenSearches = async () => {
+    try {
+        const res = await axios.get('http://localhost:5000/api/players');
+        return res.data.filter(element => element.searches > 10);
+    } catch(err) {
+        return {
+            error: { message: err.message }
+        }
+    }
+}
