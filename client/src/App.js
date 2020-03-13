9938fafda05dec0e29e2ec14414471212c153cf0
fbzr <fabricio.spb@gmail.com>
2020-03-13 09:34:50 -0700

    apply create dark mode toggle, and add it to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react';
 import SearchesList from './components/SearchesList';
 import { Container } from '@material-ui/core';
-import { useLocalStorage } from './hooks/useLocalStorage';
+import DarkModeToggle from './components/DarkModeToggle';
+import './App.scss';
 
 function App() {
-  const [test, setTest] = useLocalStorage('test', false);
-  
   return (
     <Container>
       <h1>App</h1>
+      <DarkModeToggle />
       <SearchesList />
     </Container>
   );
